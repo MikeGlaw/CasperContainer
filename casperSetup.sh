fae47c4b7696edd1d93c885c66b5db0532e83cb4
Michael Greenlaw <30806896+MikeGlaw@users.noreply.github.com>
2018-07-02 16:41:04 -0400

    Update casperSetup.sh

--- casperSetup.sh
+++ casperSetup.sh
@@ -3,11 +3,12 @@
 
 wget https://ftp.mozilla.org/pub/firefox/releases/59.0.2/linux-x86_64/en-US/firefox-59.0.2.tar.bz2
 tar -xvf firefox-59.0.2.tar.bz2
-sudo rm –r "/usr/lib/*firefox*"
+alias rm="rm -r"
+sudo rm "/usr/lib/*firefox*"
 sudo rm "/usr/bin/*firefox*"
 sudo mv firefox /usr/lib/
 sudo ln –sf /usr/lib/firefox/firefox /usr/bin/firefox
-sudo rm –r /usr/lib/firefox/*update*
+sudo rm /usr/lib/firefox/*update*
 export SLIMERJSLAUNCHER="/usr/bin/firefox:$PATH"
 
 sudo apt update -y
