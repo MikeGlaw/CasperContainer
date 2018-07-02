0440d26e271e3e0e3c8ea2c6536b1a0918daf5cc
Michael Greenlaw <30806896+MikeGlaw@users.noreply.github.com>
2018-07-02 16:14:56 -0400

    Update casperSetup.sh

--- casperSetup.sh
+++ casperSetup.sh
@@ -8,6 +8,8 @@ sudo rm /usr/bin/*firefox*
 sudo mv firefox /usr/lib/
 sudo ln –sf /usr/lib/firefox/firefox /usr/bin/firefox
 sudo rm –r /usr/lib/firefox/*update*
+export SLIMERJSLAUNCHER="/usr/bin/firefox:$PATH"
+
 sudo apt update -y
 sudo apt install -y libstdc++6 libfontconfig libfontconfig1-dev libfreetype6 git python phantomjs libc6 libstdc++6 libgcc1 libgtk2.0-0 libasound2 libxrender1 libdbus-glib-1-2 wget
 wget https://download.slimerjs.org/releases/1.0.0/slimerjs-1.0.0.tar.bz2
@@ -17,5 +19,5 @@ sudo ln -sf `pwd`/slimerjs /usr/local/bin/slimerjs
 sudo sed -i "/MaxVersion=/c\MaxVersion=59.*" application.ini
 git clone git://github.com/casperjs/casperjs.git
 sudo ln -sf `pwd`/casperjs/bin/casperjs /usr/local/bin/casperjs
-export SLIMERJSLAUNCHER=/usr/bin/firefox
+
 sudo shutdown -r now
